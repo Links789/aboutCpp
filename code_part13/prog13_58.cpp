0d00d90564a3862721076a1ab57af0884d901cb1
Links789 <85124456+Links789@users.noreply.github.com>
2021-12-17 14:24:43 +0800

    Add files via upload

---
+++ code_part13/prog13_58.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Foo
+{
+public:
+    Foo sorted() &&;
+    Foo sorted() const &;
+
+private:
+    std::vector<int> data;
+};
+
+Foo Foo::sorted() &&
+{
+    sort(data.begin(), data.end());
+    std::cout << " && " << std::endl;
+    return *this;
+};
+
+Foo Foo::sorted() const &
+{
+    std::cout << "const &" << std::endl;
+
+    return Foo(*this).sorted();
+}
+
+int main()
+{
+    Foo().sorted();
+    Foo f;
+    f.sorted();
+}
